92f3dfb5332f055477d707e329edbffbbd3960c8
Abhinay Ambati <151802585+AbhinayAmbati@users.noreply.github.com>
2024-11-02 18:12:23 +0000

    Update NavbarItem.jsx

--- src/app/components/NavbarItem.jsx
+++ src/app/components/NavbarItem.jsx
@@ -1,22 +1,31 @@
-"use client"
+"use client";
 
 import Link from 'next/link';
-import React from 'react'
+import React, { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
 
-const NavbarItem = ({title, param}) => {
-
-    const searchParams = useSearchParams();
-    const genre = searchParams.get('genre');
+const NavbarItem = ({ title, param }) => {
+  const searchParams = useSearchParams();
+  const genre = searchParams.get('genre');
 
   return (
     <div>
-        <Link
-        className={`hover:text-amber-600 font-semibold
-            ${genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''}`}
-         href={`/?genre=${param}`} >{title}</Link>
+      <Link
+        className={`hover:text-amber-600 font-semibold ${
+          genre === param ? 'underline underline-offset-8 decoration-4 decoration-amber-500 rounded-lg' : ''
+        }`}
+        href={`/?genre=${param}`}
+      >
+        {title}
+      </Link>
     </div>
-  )
-}
+  );
+};
 
-export default NavbarItem;
+const NavbarItemWithSuspense = (props) => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <NavbarItem {...props} />
+  </Suspense>
+);
+
+export default NavbarItemWithSuspense;
